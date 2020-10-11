68bb78655871d5ff1d0d00e89b9b5af630bfcab3
kezhen <kezhen@sinosoft.com.cn>
2020-10-11 22:53:40 +0800

    加了sout

--- day09-code/src/demo03/override.java
+++ day09-code/src/demo03/override.java
@@ -3,5 +3,7 @@ package demo03;
 public class override {
     public static void main(String[] args) {
         System.out.println("asdfasdfasd");
+        System.out.println("admin");
+        System.out.println("冯硕");
     }
 }
