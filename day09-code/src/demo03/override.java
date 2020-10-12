1ad7cd99da62b8ab3a9348d474c9542b36dace70
kezhen <kezhen@sinosoft.com.cn>
2020-10-13 00:01:23 +0800

    string + i + 1

--- day09-code/src/demo03/override.java
+++ day09-code/src/demo03/override.java
@@ -7,5 +7,16 @@ public class override {
         System.out.println("冯硕");
         System.out.println("tijiao");
         System.out.println("自由人");
+        String[] tstr = new String[3];
+        tstr[0] = "admin";
+        tstr[1] = "封妖";
+        tstr[2] = "addddd";
+        System.out.println(tstr);
+        for (int i = 0; i < tstr.length; i++) {
+            System.out.println(tstr[i]);
+        }
+        String str = "admin";
+        int i = 1;
+        System.out.println(str + (i + 1));
     }
 }
