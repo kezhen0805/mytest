4e36f4b48528c767e414a967444363cb941b072f
kezhen <kezhen@sinosoft.com.cn>
2021-06-30 17:38:55 +0800

    string + i + 1

--- day09-code/src/cn/itcast/day09/Teacher.java
+++
@@ -1,7 +0,0 @@
-package cn.itcast.day09;
-
-public class Teacher {
-    public static void main(String[] args) {
-        System.out.println("admin");
-    }
-}
---
+++ java_test/src/com/sinosoft/test/deom01_mkdir.java
@@ -0,0 +1,14 @@
+package com.sinosoft.test;
+
+import java.io.File;
+
+public class deom01_mkdir {
+    public static void main(String[] args) {
+        System.out.println("D:\\admin\\1234\\asdfasdfasdfasdf.xml");
+        File file = new File("D:\\admin\\1234\\asdfasdfasdfasdf.xml");
+        String dir = "D:\\admin\\1234\\5678";
+        removeDir.isChartPathExist(dir);
+
+
+    }
+}
